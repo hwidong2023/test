c23a428cf4d9dff0b0f348e96dcb5159b2e86a8d
hwidong2023 <hwidong2023@gmail.com>
2024-02-13 09:48:41 +0900

    scanner test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("프로그램 시작");
+        Scanner sc = new Scanner(System.in);
+        String test = sc.nextLine();
+        System.out.println(test);
+        sc.close();
+        System.out.println("프로그램 시작");
+    }
+}
